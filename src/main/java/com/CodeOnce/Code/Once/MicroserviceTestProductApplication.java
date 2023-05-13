db4f2bcb744b894fd2186d2731f4a64669a7d2a4
Mohamed said taleb <MohamedSaid.Taleb@hydatis.com>
2023-05-13 11:48:11 +0100

    create project

---
+++ src/main/java/com/CodeOnce/Code/Once/MicroserviceTestProductApplication.java
@@ -0,0 +1,13 @@
+package com.CodeOnce.Code.Once;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTestProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTestProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodeOnce/Code/Once/MicroserviceTestProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeOnce.Code.Once;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTestProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
